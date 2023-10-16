70e87b0a4e21cf2035d379b212a48b4261c1d309
Kiyotaka Takizuka <Kiyotaka.Takizuka@jp.nttdata.com>
2023-10-16 22:38:00 +0900

    :technologist: 静的解析のためESLintの設定を追加

---
+++ .eslintrc.js
@@ -0,0 +1,42 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "prettier",
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
+    },
+    {
+      files: ["*.{js,cjs}"],
+      rules: {
+        "@typescript-eslint/no-var-requires": "off",
+      },
+    },
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
+  plugins: ["@typescript-eslint", "react"],
+  rules: {},
+  ignorePatterns: ["build", "node_modules"],
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+};
